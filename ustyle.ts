52c67bfdf34348d62d4bd381563b464adbd34089
Einar Boson <einar.boson@gmail.com>
2021-01-12 01:08:30 +0100

    Basic command line functionality in place

---
+++ ustyle.ts
@@ -0,0 +1,143 @@
+import { Command } from "https://deno.land/x/cliffy@v0.16.0/command/mod.ts";
+import { allTextStyles, style, TextStyle, unstyle } from "./mod.ts";
+import * as Colors from "https://deno.land/std@0.74.0/fmt/colors.ts";
+
+export async function main() {
+  const equals = (a: string) => (b: string) => a === b;
+  const hasFlag = (arg: string) => Deno.args.some(equals(arg));
+  const getFlagArg = (arg: string): string | undefined =>
+    Deno.args[Deno.args.indexOf(arg) + 1];
+  const getStyleFlags = (index: number) => {
+    const styles = [];
+    while (Deno.args[index].startsWith("-")) {
+      styles.push(Deno.args[index]);
+      index++;
+    }
+    return styles;
+  };
+
+  if (hasFlag("--help") || Deno.args.length === 0) {
+    printUsage();
+  } else if (hasFlag("--list")) {
+    const example = getFlagArg("--list") ?? "Lorem Ipsum Dolor Sit Amet 1234";
+    listStyles(example);
+  } else if (hasFlag("--template")) {
+    let template = getFlagArg("--template");
+    if (template === "-") {
+      template = new TextDecoder().decode(await Deno.readAll(Deno.stdin));
+    }
+    console.log("template is not implemented yet :/");
+  } else {
+    let index = 0;
+    const output: string[] = [];
+    do {
+      const styleFlags = getStyleFlags(index);
+      const text = Deno.args[index + styleFlags.length];
+      index += styleFlags.length + 1;
+      let styleName: TextStyle;
+      try {
+        styleName = optionsToStyle(styleFlags);
+        output.push(style(text, styleName));
+      } catch (msg) {
+        console.log("Error", msg);
+        Deno.exit();
+      }
+    } while (index < Deno.args.length);
+    console.log(output.join(""));
+  }
+}
+
+/** ordered in the order they always appears in compounds */
+const styleElements = [
+  "sans-serif",
+  "bold",
+  "italic",
+  "fraktur",
+  "monospace",
+  "script",
+  "double-struck",
+] as const;
+
+type Style = typeof styleElements[number];
+
+const styleShortOptions: Record<string, Style> = {
+  "b": "bold",
+  "i": "italic",
+  "c": "script",
+  "f": "fraktur",
+  "d": "double-struck",
+  "s": "sans-serif",
+  "m": "monospace",
+};
+
+function optionsToStyle(opts: string[]): TextStyle {
+  const styles: Style[] = [];
+  for (const opt of opts) {
+    if (opt.startsWith("--")) {
+      styles.push(validateLongOption(opt));
+    } else {
+      styles.push(...translateShortOptions(opt));
+    }
+  }
+  return getStyle(styles);
+}
+
+function validateLongOption(opt: string) {
+  const name = opt.substr(2);
+  if ((<readonly string[]> styleElements).includes(name)) {
+    return name as Style;
+  } else {
+    throw `'${opt}' is not a valid style`;
+  }
+}
+function translateShortOptions(opts: string): Style[] {
+  const styles: Style[] = [];
+  for (const opt of opts.substr(1)) {
+    if (styleShortOptions[opt]) {
+      styles.push(styleShortOptions[opt]);
+    } else {
+      throw `${opt} is not a valid option (found in ${opts})`;
+    }
+  }
+  return styles;
+}
+
+function getStyle(styles: Style[]): TextStyle {
+  styles.sort((el1, el2) =>
+    styleElements.indexOf(el1) - styleElements.indexOf(el2)
+  );
+  const styleName = styles.join(" ").toUpperCase();
+  if (allTextStyles.includes(<TextStyle> styleName)) {
+    return styleName as TextStyle;
+  } else {
+    throw `There is no ${styleName} available in unicode`;
+  }
+}
+
+/**
+ * Make the option name of a style name, e.g. "BOLD ITALIC" becomes "--bold-italic"
+ * @param name the name of the style
+ */
+function opt(name: string): string {
+  return name.toLowerCase().replace(/\s+/g, ".").replace(/-/, "_");
+}
+
+function printUsage() {
+}
+function listStyles(example: string) {
+  console.log();
+  console.log(Colors.blue("Available styles are:"));
+  console.log();
+  const colWidth = Math.max(...allTextStyles.map((str) => str.length));
+  allTextStyles.forEach((styleName, i) => {
+    console.log(
+      `  --${opt(styleName).padEnd(colWidth + 2, " ")} ${
+        style(example, styleName)
+      }`,
+    );
+  });
+}
+
+if (import.meta.main) {
+  main();
+}
